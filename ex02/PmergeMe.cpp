37ef1d499359feeae0256c0771cdbe2a2472cd3f
soragogo <es377233@gmail.com>
2025-04-07 08:32:42 +0900

    (WIP) cannot compile

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -1,28 +1,4 @@
 #include "PmergeMe.hpp"
-
-// Node class constructor and destructor
-Node::Node(const int & i) : _number(i), _sorted(false) {}
-
-Node::Node(void) : _number(-1), _sorted(false) {}
-
-Node::~Node() {}
-
-// Copy constructor and assignment operator
-Node::Node(const Node & src) {
-    *this = src;
-}
-
-Node & Node::operator=(const Node & rhs) {
-    if (this != &rhs) {
-        this->_number = rhs._number;
-        this->_sorted = rhs._sorted;
-        this->_larger_pair = rhs._larger_pair;
-        this->_smaller_pair = rhs._smaller_pair;
-    }
-    return *this;
-}
-
-
 std::vector<int> calculate_jacobsthal_sequence(int n) {
     std::vector<int> sequence(n + 1);
     sequence[0] = 0;
@@ -50,7 +26,7 @@ std::vector<int> generate_indices(int count) {
     return indices;
 }
 
-void merge_insersion_sort_vector(std::vector<Node*> v, std::vector<Node*> &res){
+void merge_insersion_sort_vector(std::vector<Node_v*> v, std::vector<Node_v*> &res){
 
     if (v.size() < 2) {
         res.push_back(v[0]);
@@ -72,12 +48,12 @@ void merge_insersion_sort_vector(std::vector<Node*> v, std::vector<Node*> &res){
         return;
     }
 
-    std::vector < Node * >large;
-    std::vector < Node * >small;
-    for (std::vector<Node*>::iterator it = v.begin(); it != v.end() - 1; ++it) {
+    std::vector < Node_v * >large;
+    std::vector < Node_v * >small;
+    for (std::vector<Node_v*>::iterator it = v.begin(); it != v.end() - 1; ++it) {
         if (std::distance(it, v.begin()) % 2 == 0) {
-            std::vector<Node *>::iterator l_it;
-            std::vector<Node *>::iterator s_it;
+            std::vector<Node_v *>::iterator l_it;
+            std::vector<Node_v *>::iterator s_it;
             // count
             count_vector++;
             if ((*it)->getNumber() > (*(it + 1))->getNumber()) {
@@ -92,12 +68,12 @@ void merge_insersion_sort_vector(std::vector<Node*> v, std::vector<Node*> &res){
             large.push_back(*l_it);
         }
     }
-    Node * leaf = NULL;
+    Node_v * leaf = NULL;
     if (v.size() % 2 == 1)
         leaf = v.back();
 
     merge_insersion_sort_vector(large, res);
-    for (std::vector<Node *>::iterator it = res.begin(); it != res.end(); ++it) {
+    for (std::vector<Node_v *>::iterator it = res.begin(); it != res.end(); ++it) {
         if ((*it)->getSmallerPair() != NULL) {
             small.push_back((*it)->getSmallerPair());
             (*it)->eraseSmallerPair();
@@ -117,18 +93,18 @@ void merge_insersion_sort_vector(std::vector<Node*> v, std::vector<Node*> &res){
         }
 
         int geta = (indices[i] - 1) > small_end ? small_end-- : indices[i] - 1;
-        std::vector<Node*>::iterator it = (small.begin() + geta);
-        Node* smaller_pair = *it;
-        Node* larger_pair = smaller_pair->getLargerPair();
+        std::vector<Node_v*>::iterator it = (small.begin() + geta);
+        Node_v* smaller_pair = *it;
+        Node_v* larger_pair = smaller_pair->getLargerPair();
 
-        std::vector<Node*>::iterator res_pos;
+        std::vector<Node_v*>::iterator res_pos;
         for (res_pos = res.begin(); res_pos != res.end(); ++res_pos) {
             if (*res_pos == larger_pair) break;
         }
 
-        std::vector<Node*>::iterator begin = res.begin();
-        std::vector<Node*>::iterator end = res_pos - 1;
-        std::vector<Node*>::iterator mid;
+        std::vector<Node_v*>::iterator begin = res.begin();
+        std::vector<Node_v*>::iterator end = res_pos - 1;
+        std::vector<Node_v*>::iterator mid;
         while (begin <= end) {
             mid = begin + (end - begin) / 2;
             // count
@@ -146,36 +122,36 @@ void merge_insersion_sort_vector(std::vector<Node*> v, std::vector<Node*> &res){
 }
 
 
-void merge_insersion_sort_deque(std::deque<Node*> v, std::deque<Node*> &res){
+void merge_insersion_sort_list(std::list<Node_l*> l, std::list<Node_l*> &res){
 
-    if (v.size() < 2) {
-        res.push_back(v[0]);
+    if (l.size() < 2) {
+        res.push_back(l[0]);
         return;
     }
 
-    if (v.size() == 2) {
+    if (l.size() == 2) {
         // count
-        count_deque++;
-        if (v[0]->getNumber() > v[1]->getNumber()) {
-            res.push_back(v[1]);
-            res.push_back(v[0]);
+        count_list++;
+        if (l[0]->getNumber() > l[1]->getNumber()) {
+            res.push_back(l[1]);
+            res.push_back(l[0]);
         } else {
-            res.push_back(v[0]);
-            res.push_back(v[1]);
+            res.push_back(l[0]);
+            res.push_back(l[1]);
         }
-        v[0]->sorted();
-        v[1]->sorted();
+        l[0]->sorted();
+        l[1]->sorted();
         return;
     }
 
-    std::deque < Node * >large;
-    std::deque < Node * >small;
-    for (std::deque<Node*>::iterator it = v.begin(); it != v.end() - 1; ++it) {
-        if (std::distance(it, v.begin()) % 2 == 0) {
-            std::deque<Node *>::iterator l_it;
-            std::deque<Node *>::iterator s_it;
+    std::list < Node_l * >large;
+    std::list < Node_l * >small;
+    for (std::list<Node_l*>::iterator it = l.begin(); it != l.end() - 1; ++it) {
+        if (std::distance(it, l.begin()) % 2 == 0) {
+            std::list<Node_l *>::iterator l_it;
+            std::list<Node_l *>::iterator s_it;
             // count
-            count_deque++;
+            count_list++;
             if ((*it)->getNumber() > (*(it + 1))->getNumber()) {
                 l_it = it;
                 s_it = it + 1;
@@ -188,12 +164,12 @@ void merge_insersion_sort_deque(std::deque<Node*> v, std::deque<Node*> &res){
             large.push_back(*l_it);
         }
     }
-    Node * leaf = NULL;
-    if (v.size() % 2 == 1)
-        leaf = v.back();
+    Node_l * leaf = NULL;
+    if (l.size() % 2 == 1)
+        leaf = l.back();
 
-    merge_insersion_sort_deque(large, res);
-    for (std::deque<Node *>::iterator it = res.begin(); it != res.end(); ++it) {
+    merge_insersion_sort_list(large, res);
+    for (std::list<Node_l *>::iterator it = res.begin(); it != res.end(); ++it) {
         if ((*it)->getSmallerPair() != NULL) {
             small.push_back((*it)->getSmallerPair());
             (*it)->eraseSmallerPair();
@@ -213,22 +189,22 @@ void merge_insersion_sort_deque(std::deque<Node*> v, std::deque<Node*> &res){
         }
 
         int geta = (indices[i] - 1) > small_end ? small_end-- : indices[i] - 1;
-        std::deque<Node*>::iterator it = (small.begin() + geta);
-        Node* smaller_pair = *it;
-        Node* larger_pair = smaller_pair->getLargerPair();
+        std::list<Node_l*>::iterator it = (small.begin() + geta);
+        Node_l* smaller_pair = *it;
+        Node_l* larger_pair = smaller_pair->getLargerPair();
 
-        std::deque<Node*>::iterator res_pos;
+        std::list<Node_l*>::iterator res_pos;
         for (res_pos = res.begin(); res_pos != res.end(); ++res_pos) {
             if (*res_pos == larger_pair) break;
         }
 
-        std::deque<Node*>::iterator begin = res.begin();
-        std::deque<Node*>::iterator end = res_pos - 1;
-        std::deque<Node*>::iterator mid;
+        std::list<Node_l*>::iterator begin = res.begin();
+        std::list<Node_l*>::iterator end = res_pos - 1;
+        std::list<Node_l*>::iterator mid;
         while (begin <= end) {
             mid = begin + (end - begin) / 2;
             // count
-            count_deque++;
+            count_list++;
             if ((*mid)->getNumber() > smaller_pair->getNumber()) {
                 end = mid - 1;
             } else {
@@ -239,4 +215,52 @@ void merge_insersion_sort_deque(std::deque<Node*> v, std::deque<Node*> &res){
         smaller_pair->sorted();
         res.insert(begin, smaller_pair);
     }
+}
+
+
+//Node_v
+
+// Node class constructor and destructor
+Node_v::Node_v(const int & i) : _number(i), _sorted(false) {}
+
+Node_v::Node_v(void) : _number(-1), _sorted(false) {}
+
+Node_v::~Node_v() {}
+
+// Copy constructor and assignment operator
+Node_v::Node_v(const Node_v & src) {
+    *this = src;
+}
+
+Node_v & Node_v::operator=(const Node_v & rhs) {
+    if (this != &rhs) {
+        this->_number = rhs._number;
+        this->_sorted = rhs._sorted;
+        this->_larger_pair = rhs._larger_pair;
+        this->_smaller_pair = rhs._smaller_pair;
+    }
+    return *this;
+}
+
+//Node_l
+
+Node_l::Node_l(const int & i) : _number(i), _sorted(false) {}
+
+Node_l::Node_l(void) : _number(-1), _sorted(false) {}
+
+Node_l::~Node_l() {}
+
+// Copy constructor and assignment operator
+Node_l::Node_l(const Node_l & src) {
+    *this = src;
+}
+
+Node_l & Node_l::operator=(const Node_l & rhs) {
+    if (this != &rhs) {
+        this->_number = rhs._number;
+        this->_sorted = rhs._sorted;
+        this->_larger_pair = rhs._larger_pair;
+        this->_smaller_pair = rhs._smaller_pair;
+    }
+    return *this;
 }
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -2,21 +2,20 @@
 # define PMERGEME_HPP
 
 #include <vector>
-#include <deque>
 #include <list>
 #include <iostream>
 
-class Node {
+class Node_v {
     public:
         // Constructor and Destructor
-        Node(const int & i);
-        ~Node();
+        Node_v(const int & i);
+        ~Node_v();
 
         // Member functions
-        inline Node * & getLargerPair() { return _larger_pair; };
-        inline Node * & getSmallerPair() { return _smaller_pair.back(); };
-        inline void setLargerPair(Node * n) {_larger_pair = n;};
-        inline void setSmallerPair(Node * n) {_smaller_pair.push_back(n);};
+        inline Node_v * & getLargerPair() { return _larger_pair; };
+        inline Node_v * & getSmallerPair() { return _smaller_pair.back(); };
+        inline void setLargerPair(Node_v * n) {_larger_pair = n;};
+        inline void setSmallerPair(Node_v * n) {_smaller_pair.push_back(n);};
         inline void eraseSmallerPair() {_smaller_pair.pop_back();}
         inline int const & getNumber() {return _number;};
         inline void sorted() {_sorted = true;};
@@ -25,21 +24,49 @@ class Node {
     private:
         int _number;
         bool _sorted;
-        std::list<Node *> _smaller_pair;
-        Node* _larger_pair;
+        std::vector<Node_v *> _smaller_pair;
+        Node_v* _larger_pair;
 
         // Copy constructor and assignment operator
-        Node(void);
-        Node(const Node & src);
-        Node & operator=(const Node & rhs);
+        Node_v(void);
+        Node_v(const Node_v & src);
+        Node_v & operator=(const Node_v & rhs);
 };
 
-void merge_insersion_sort_vector(std::vector<Node*> v, std::vector<Node*> &res);
-void merge_insersion_sort_deque(std::deque<Node*> v, std::deque<Node*> &res);
+class Node_l {
+    public:
+        // Constructor and Destructor
+        Node_l(const int & i);
+        ~Node_l();
+
+        // Member functions
+        inline Node_l * & getLargerPair() { return _larger_pair; };
+        inline Node_l * & getSmallerPair() { return _smaller_pair.back(); };
+        inline void setLargerPair(Node_l * n) {_larger_pair = n;};
+        inline void setSmallerPair(Node_l * n) {_smaller_pair.push_back(n);};
+        inline void eraseSmallerPair() {_smaller_pair.pop_back();}
+        inline int const & getNumber() {return _number;};
+        inline void sorted() {_sorted = true;};
+        inline bool isSorted() {return _sorted;};
+
+    private:
+        int _number;
+        bool _sorted;
+        std::list<Node_l *> _smaller_pair;
+        Node_l* _larger_pair;
+
+        // Copy constructor and assignment operator
+        Node_l(void);
+        Node_l(const Node_l & src);
+        Node_l & operator=(const Node_l & rhs);
+};
+
+void merge_insersion_sort_vector(std::vector<Node_v*> v, std::vector<Node_v*> &res);
+void merge_insersion_sort_list(std::list<Node_l*> l, std::list<Node_l*> &res);
 
 // count
 extern size_t count_vector;
-extern size_t count_deque;
+extern size_t count_list;
 
 // colors
 # define END             "\033[0m"
