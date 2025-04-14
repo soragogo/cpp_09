9e64b3d52cfeca95d40a0768e2f274ecf81864e4
soragogo <es377233@gmail.com>
2025-04-14 22:30:18 +0900

    signal

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,6 +1,17 @@
 #include "RPN.hpp"
 
+#include <csignal>
+
+
+void signalHandler(int signalNumber) {
+    std::cerr << "signal " << signalNumber << " received" << std::endl;
+    exit(signalNumber);
+}
+
 int main(int argc, char * argv[]) {
+
+    signal(SIGFPE, signalHandler);
+
     if (argc != 2) {
         std::cerr << "You must pass one argument." << std::endl;
         return 1;
