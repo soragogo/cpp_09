06eeefc9b2bde4d173cc9be22becbe4b446f703f
soragogo <es377233@gmail.com>
2025-04-14 21:18:39 +0900

    cerr

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -11,6 +11,6 @@ int main(int argc, char * argv[]) {
         rpn.caliculate();
     }
     catch (std::exception & e){
-        std::cout << e.what() << std::endl;
+        std::cerr << e.what() << std::endl;
     }
 }
